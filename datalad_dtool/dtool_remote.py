8dddf2cccfd7de7d864d2c7bb72c80bf10e34aa0
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2024-10-10 16:01:27 +0200

    ENH: read-only test working for dtool special remote

--- datalad_dtool/dtool_remote.py
+++ datalad_dtool/dtool_remote.py
@@ -5,7 +5,7 @@ from annexremote import Master
 from annexremote import SpecialRemote
 from annexremote import RemoteError
 
-from dtoolcore import DataSet
+from dtoolcore import DataSet, ProtoDataSet, DtoolCoreTypeError
 
 logger = logging.getLogger(__name__)
 
@@ -33,31 +33,56 @@ class DtoolRemote(SpecialRemote):
         # prepare to be used, eg. open TCP connection, authenticate with the server etc.
         # raise RemoteError if not ready to use
         self.uri = self.annex.getconfig("uri")
-        self.dtool_dataset = DataSet.from_uri(self.uri)
+        try:
+            self.dtool_dataset = DataSet.from_uri(self.uri)
+            logger.debug("Dataset uri=%s frozen, immutable.", self.uri)
+        except DtoolCoreTypeError as exc:
+            logger.warning(exc)
+            self.dtool_dataset = ProtoDataSet.from_uri(self.uri)
 
     def transfer_retrieve(self, key, filename):
         # get the file identified by `key` and store it to `filename`
         # raise RemoteError if the file couldn't be retrieved
-        try:
-            fpath = self.dtool_dataset.item_content_abspath(key)
-        except Exception as e:
-            raise RemoteError(e)
-        shutil.copyfile(fpath, filename)
+
+        if isinstance(self.dtool_dataset, ProtoDataSet):
+            self.dtool_dataset.freeze()
+            self.dtool_dataset = DataSet.from_uri(self.uri)
+
+        manifest = self.dtool_dataset.generate_manifest()
+        for uuid, entry in manifest['items'].items():
+            if entry["relpath"] == key:
+                try:
+                    fpath = self.dtool_dataset.item_content_abspath(uuid)
+                except Exception as e:
+                    raise RemoteError(e)
+                shutil.copyfile(fpath, filename)
+                return
+
+        raise RemoteError()
 
     def checkpresent(self, key):
         # return True if the key is present in the remote
         # return False if the key is not present
         # raise RemoteError if the presence of the key couldn't be determined, eg. in case of connection error
-        if key in self.dtool_dataset.identifiers:
-            return True
-        else:
-            return False
+        logger.debug("Looking for item %s in dataset %s", key, self.uri)
+
+        if isinstance(self.dtool_dataset, ProtoDataSet):
+            self.dtool_dataset.freeze()
+            self.dtool_dataset = DataSet.from_uri(self.uri)
+
+        manifest = self.dtool_dataset.generate_manifest()
+        for _, entry in manifest['items'].items():
+            if entry["relpath"] == key:
+                return True
+
+        return False
 
     def claimurl(self, url: str) -> bool:
-        return url.startswith("dtool:")
+        return url.startswith("dtool")
 
     def checkurl(self, url: str) -> bool:
-        return url.startswith("dtool:")
+        return url.startswith("dtool")
+        # TODO: implement more sophisticated checking on URL
 
     def getcost(self) -> int:
         # This is a very expensive remote
