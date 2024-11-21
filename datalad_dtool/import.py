97f8b7d5bef6b36fc01f1e23e14164015f714201
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2024-11-21 14:42:27 +0100

    ENH: working DtoolImport

--- datalad_dtool/import.py
+++ datalad_dtool/import.py
@@ -3,7 +3,7 @@
 __docformat__ = "restructuredtext"
 import logging
 
-from typing import Literal
+from typing import Literal, Optional
 
 from datalad.distribution.dataset import (
     EnsureDataset,
@@ -18,7 +18,8 @@ from datalad.support.constraints import EnsureNone, EnsureStr, EnsureChoice
 from datalad.support.param import Parameter
 
 import datalad_dtool.dtool_remote
-# import datalad_cds.spec
+
+from dtoolcore import DataSet
 from dtoolcore.utils import sanitise_uri
 
 logger = logging.getLogger("datalad.dtool.import")
@@ -28,55 +29,80 @@ logger = logging.getLogger("datalad.dtool.import")
 @build_doc
 # all commands must be derived from Interface
 class DtoolImport(Interface):
-    """Downloads specified datasets from the CDS data store"""
+    """Import a dtool dataset into a datalad dataset"""
 
     _params_ = dict(
+        uri=Parameter(
+            args=("uri",),
+            metavar="URI",
+            doc="""dtool URI of dtool dataset to import.""",
+            constraints=EnsureStr() | EnsureNone()),
         dataset=Parameter(
-            doc="""Specify the dataset to import. 
-                   Constraints: Value must be a Dataset or a
-                   valid identifier of a Dataset (e.g. a path) or value must be 
-                   NONE.""",
+            args=("-d", "--dataset"),
+            doc="""specify the datalad dataset to add files to. If no 
+                   datalad dataset is given, an attempt is made to identify the dataset 
+                   based on the current working directory. Use 
+                   [CMD: --nosave CMD][PY: save=False PY] to
+                   prevent adding files to the dataset.""",
+            constraints=EnsureDataset() | EnsureNone()),
+        path=Parameter(
+            args=("-O", "--path"),
+            doc="""Relative target path to import to within datalad dataset.""",
+            constraints=EnsureStr() | EnsureNone(),
         ),
-        base_uri=Parameter(
-            args=("-uri", "--base_uri"),
-            metavar="PATH",
-            doc="""specify the dataset to add files to. If no dataset is given,
-            an attempt is made to identify the dataset based on the current
-            working directory. Use [CMD: --nosave CMD][PY: save=False PY] to
-            prevent adding files to the dataset.""",
-        ),
-        name=Parameter(
-            args=("-n", "--name"),
-            doc="""Name of datalad_dataset.""",
-        ),
-        missing_content=Parameter(
-            args=("--missing-content",),
-            doc="""By default, any discovered file with missing content will
-                   result in an error and the export is aborted. Setting this to
-                   'continue' will issue warnings instead of failing on error.
-                   The value 'ignore' will only inform about problem at the
-                   'debug' log level. The latter two can be helpful when
-                   generating a TAR archive from a dataset where some file
-                   content is not available locally.""",
-            constraints=EnsureChoice("error", "continue", "ignore")),
+        save=nosave_opt,
+        message=save_message_opt,
     )
 
     @staticmethod
     @datasetmethod(name="import_dtool")
     @eval_results
     def __call__(
-        base_uri:None,
+        uri: str,
         *,
-        missing_content='error',
-        name=None):
-        if base_uri is None:
-            base_uri = abspath(curdir)
-        sanitised_base_uri = sanitise_uri(base_uri)
-        yield get_status_dict(action="import-dtool", status="ok")
+        dataset: Optional[str] = None,
+        path: Optional[str] = None,
+        message: Optional[str] = None,
+        save: bool = True):
+        # if uri is None:
+        #    uri = abspath(curdir)
+        sanitised_uri = sanitise_uri(uri)
+        logger.debug("Sanitized dtool dataset URI: %s", sanitised_uri)
+
+
+        ds = require_dataset(dataset, check_installed=True)
+        ensure_special_remote_exists_and_is_enabled(
+            repo=ds.repo, remote="dtool", uri=sanitised_uri)
+
+        if path is None:
+            pathobj = ds.pathobj
+        else:
+            pathobj = ds.pathobj / path
+
+        dtool_dataset = DataSet.from_uri(sanitised_uri)
+        manifest = dtool_dataset.generate_manifest()
+        for uuid, entry in manifest['items'].items():
+            relpath = entry["relpath"]
+            file_pathobj = pathobj / relpath
+            dtool_item_uri = f'dtool:{uri}/{uuid}'
+            logger.debug(
+                "Import dtool dataset URI '%s' item '%s' to path '%s' within '%s'",
+                sanitised_uri, uuid, relpath, pathobj)
+            ds.repo.add_url_to_file(file_pathobj, dtool_item_uri)
+
+        if save:
+            msg = (
+                message
+                if message is not None
+                else "[DATALAD] import from dtool dataset '{}'".format(uri)
+            )
+            yield ds.save(pathobj, message=msg)
+
+        yield get_status_dict(action="import-dtool", ds=ds, status="ok")
 
 
 def ensure_special_remote_exists_and_is_enabled(
-    repo: AnnexRepo, remote: Literal["dtool_remote"]
+    repo: AnnexRepo, remote: Literal["dtool"], uri: str,
 ) -> None:
     """Initialize and enable the dtool special remote, if it isn't already.
 
@@ -88,6 +114,7 @@ def ensure_special_remote_exists_and_is_enabled(
 
     name = repo.get_special_remotes().get(uuid, {}).get("name")
     if not name:
+        logger.debug("no suitable special remote found, initialize dtool remote")
         repo.init_remote(
             remote,
             [
@@ -96,6 +123,7 @@ def ensure_special_remote_exists_and_is_enabled(
                 "autoenable=true",
                 "externaltype={}".format(remote),
                 "uuid={}".format(uuid),
+                "uri={}".format(uri)
             ],
         )
     elif repo.is_special_annex_remote(name, check_if_known=False):
